f720e736ddce5317cac64075d9c27a7eaf87e0c1
thehamzajunaid <74147991+thehamzajunaid@users.noreply.github.com>
2025-10-19 02:46:46 +0000

    Deploying to gh-pages from @ thehamzajunaid/thehamzajunaid.github.io@90866ead38cb85236a439342a11cc82d15ff84e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-what-actually-is-acid-in-databases",
+        },{id: "post-how-i-moved-an-azure-virtual-machine-between-resource-groups-of-different-subscriptions-without-downtime",
+        
+          title: 'How I Moved an Azure Virtual Machine Between Resource Groups of different Subscriptions(Without... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "Blog",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@hamzajunaid/how-i-moved-an-azure-virtual-machine-between-resource-groups-of-different-subscriptions-without-f77b3a83f151", "_blank");
+          
+        },
+      },{id: "post-what-actually-is-acid-in-databases",
         
           title: 'What actually is ACID in databases? <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
